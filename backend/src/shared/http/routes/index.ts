994e663119958a00f168af510cbc1d2c908e731e
Chriszao <chris.f.assis18@gmail.com>
2021-10-10 17:46:36 -0300

    feat: Add product route to index routes

--- backend/src/shared/http/routes/index.ts
+++ backend/src/shared/http/routes/index.ts
@@ -1,9 +1,8 @@
 import { Router } from 'express';
+import productsRouter from '@modules/products/routes/products.routes';
 
 const routes = Router();
 
-routes.get('/', (request, response) => {
-  response.json({ message: 'Hello world!' });
-});
+routes.use('/products', productsRouter);
 
 export default routes;
