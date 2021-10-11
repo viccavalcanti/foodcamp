d90a7019c98fc3a6eae4c43b48c7152d2f8ee58a
Chriszao <chris.f.assis18@gmail.com>
2021-10-10 21:02:57 -0300

    feat: Add update user service

---
+++ backend/src/modules/users/services/UpdateUserService.ts
@@ -0,0 +1,37 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/User';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+
+interface IUser {
+  id: string;
+  name: string;
+  email: string;
+}
+
+class UpdateUserService {
+  public async execute({ id, name, email }: IUser): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    const user = await usersRepository.findById(id);
+
+    if (!user) {
+      throw new AppError('User not found', 404);
+    }
+
+    const emailExists = await usersRepository.findByEmail(email);
+
+    if (emailExists) {
+      throw new AppError('Email is already in use');
+    }
+
+    user.name = name;
+    user.email = email;
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default UpdateUserService;
