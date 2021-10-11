b0ba7efbb77c5a67df6214c134bb049f52e1efa2
Chriszao <chris.f.assis18@gmail.com>
2021-10-10 23:24:47 -0300

    feat: Add user routes

---
+++ backend/src/modules/users/routes/user.routes.ts
@@ -0,0 +1,43 @@
+import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
+import UsersController from '../controllers/UsersController';
+
+const usersRouter = Router();
+
+const usersController = new UsersController();
+
+usersRouter.get('/', usersController.index);
+
+usersRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  usersController.show,
+);
+
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  usersController.create,
+);
+
+usersRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().required(),
+    },
+  }),
+  usersController.update,
+);
+
+export default usersRouter;
