1486a7ef44c575225794d9b9c95a7e03abee181a
Chriszao <chris.f.assis18@gmail.com>
2021-10-12 20:14:24 -0300

    refactor: refactor import

--- backend/src/modules/users/routes/user.routes.ts
+++ backend/src/modules/users/routes/user.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { celebrate, Joi, Segments } from 'celebrate';
+import isAuthenticated from '@shared/http/middlewares/isAuthenticated';
 import UsersController from '../controllers/UsersController';
-import isAuthenticated from '../middlewares/isAuthenticated';
 
 const usersRouter = Router();
 
