7a84682bbf5cdbea05139aa771eb4f5beadfa059
Chriszao <chris.f.assis18@gmail.com>
2021-10-13 19:24:52 -0300

    feat: Create static serve files

--- backend/src/shared/http/server.ts
+++ backend/src/shared/http/server.ts
@@ -5,6 +5,7 @@ import cors from 'cors';
 import 'dotenv/config';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
+import uploadConfig from '@config/upload';
 import 'reflect-metadata';
 import routes from './routes';
 
@@ -12,6 +13,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/files', express.static(uploadConfig.directory));
 
 app.use(routes);
 
