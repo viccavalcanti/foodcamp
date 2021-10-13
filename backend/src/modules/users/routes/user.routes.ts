209e9c90e941d61d9887f65a00541bda82e86606
Chriszao <chris.f.assis18@gmail.com>
2021-10-13 19:24:14 -0300

    feat: Apply middleware to upload images for user avatar

--- backend/src/modules/users/routes/user.routes.ts
+++ backend/src/modules/users/routes/user.routes.ts
@@ -1,11 +1,17 @@
 import { Router } from 'express';
 import { celebrate, Joi, Segments } from 'celebrate';
 import isAuthenticated from '@shared/http/middlewares/isAuthenticated';
+import multer from 'multer';
+import uploadConfig from '@config/upload';
 import UsersController from '../controllers/UsersController';
+import UserAvatarController from '../controllers/UserAvatarController';
 
 const usersRouter = Router();
 
 const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
+
+const upload = multer(uploadConfig);
 
 usersRouter.get('/', isAuthenticated, usersController.index);
 
@@ -41,6 +47,13 @@ usersRouter.put(
   }),
   isAuthenticated,
   usersController.update,
+);
+
+usersRouter.patch(
+  '/avatar',
+  isAuthenticated,
+  upload.single('avatar'),
+  userAvatarController.update,
 );
 
 export default usersRouter;
