6e0de5e56c1e3baa780ba8307ee73613ab0b7700
Chriszao <chris.f.assis18@gmail.com>
2021-10-17 12:50:21 -0300

    feat: Add password routes to index

--- backend/src/shared/http/routes/index.ts
+++ backend/src/shared/http/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import productsRouter from '@modules/products/routes/products.routes';
 import usersRouter from '@modules/users/routes/user.routes';
 import sessionsRouter from '@modules/users/routes/session.routes';
+import passwordRouter from '@modules/users/routes/password.routes';
 
 const routes = Router();
 
 routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
+routes.use('/password', passwordRouter);
 
 export default routes;
