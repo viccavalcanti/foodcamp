6c1a88f5f7f675502bc7b5056f119cf9efac666a
Chriszao <chris.f.assis18@gmail.com>
2021-10-18 08:06:38 -0300

    feat: Implement Password Recovery Email Sending with Ethereal

--- backend/src/modules/users/services/SendForgotPasswordEmailService.ts
+++ backend/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,6 +1,7 @@
 import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
 import EtherealMail from '@config/mail/EtherealMail';
+import path from 'path';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
 import UserTokensRepository from '../typeorm/repositories/UserTokensRepository';
 
@@ -19,11 +20,28 @@ class SendForgotPasswordEmailService {
       throw new AppError('User does not exist');
     }
 
-    const token = await userTokensRepository.generateToken(user.id);
+    const { token } = await userTokensRepository.generateToken(user.id);
+
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs',
+    );
 
     await EtherealMail.sendMail({
-      to: email,
-      body: `Solicitação de recuperação de senha recebida: ${token?.token}`,
+      to: {
+        name: user.name,
+        email,
+      },
+      subject: '[Foodcamp] Recuperação de Senha',
+      templateData: {
+        fileTemplate: forgotPasswordTemplate,
+        variables: {
+          name: user.name,
+          link: `http://localhost:${process.env.PORT}/reset_password?token=${token}`,
+        },
+      },
     });
   }
 }
